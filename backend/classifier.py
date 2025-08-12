045a1a480054a8c068daaaf4800e6dab44f15526
Ihor Bachuk <igor.bachuk04@gmail.com>
2025-08-12 03:58:29 +0300

    add model and classifier

---
+++ backend/classifier.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+
+MODEL_PATH = "static/models/cat_or_loaf.h5"
+model = tf.keras.models.load_model(MODEL_PATH)
+
+
+def preprocess(model, image_path):
+    image_size = tuple(model.input_shape[1:3])
+    image = tf.keras.utils.load_img(image_path, target_size=image_size)
+    x = tf.keras.utils.img_to_array(image)
+    x = tf.expand_dims(x, 0)
+    return x
+
+def classify(model, image_path):
+    image = preprocess(model, image_path)
+    score = float(model.predict(image, verbose=0)[0][0])  # sigmoid prob of the positive class
+
+    label = "loaf" if score >= 0.5 else "cat"
+    prob = score if label == "loaf" else 1.0 - score
+    return label, prob
+
+if __name__ == "__main__":
+    print("Expected cat: \n", classify(model, "static/images/my_cat.jpg"))
+    print("Expected loaf: \n", classify(model, "static/images/my_loaf.jpg"))
